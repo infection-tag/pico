4e7517d33ed01b6797d622f02010cfcd3884cf84
NiKo Kompella <63625756+infection-tag@users.noreply.github.com>
2024-07-22 14:36:24 -0700

    Update sketch.ino

--- the-bin-2/sketch.ino
+++ the-bin-2/sketch.ino
@@ -16,15 +16,44 @@ TM1637 tm(CLK, DIO);
 void setup() {
   // put your setup code here, to run once:
 
+  Serial1.begin(115200);
+
   tm.init();
   tm.set(BRIGHT_TYPICAL);
-  Serial1.begin(115200);
-  Serial1.println("Hello, Raspberry Pi Pico W!");
+
+  if(!rtc.begin()) {
+    Serial.println("No RTC Module Attached."); 
+    Serial.flush();
+    abort();
+  }
+
+  // Serial1.println("Hello, Raspberry Pi Pico W!");
 }
 
 unsigned int counter = 0;
 
 void loop() {
+  DateTime now = rtc.now();
+
+  tm.display(0, ((now.hour(), DEC) / 10) % 10);
+  tm.display(1, (now.hour(), DEC) % 10);
+  tm.display(2, ((now.minute(), DEC) / 10) % 10);
+  tm.display(3, (now.minute(), DEC) % 10);
+
+  // print time, for debugging 
+  Serial.print("Current time: ");
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
   // put your main code here, to run repeatedly:
   delay(1); // this speeds up the simulation
 
