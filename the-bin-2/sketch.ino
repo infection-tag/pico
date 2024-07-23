395078809fc3220a939b95f261011f6ee724a862
NiKo Kompella <63625756+infection-tag@users.noreply.github.com>
2024-07-22 20:01:18 -0700

    Update sketch.ino

--- the-bin-2/sketch.ino
+++ the-bin-2/sketch.ino
@@ -21,52 +21,60 @@ void setup() {
   tm.init();
   tm.set(BRIGHT_TYPICAL);
 
-  if(!rtc.begin()) {
-    Serial.println("No RTC Module Attached."); 
-    Serial.flush();
-    abort();
-  }
+  rtc.begin();
+
+  // if (! rtc.isrunning()) {
+  //   Serial.println("RTC is NOT running, let's set the time!");
+  //   // When time needs to be set on a new device, or after a power loss, the
+  //   // following line sets the RTC to the date & time this sketch was compiled
+  //   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //   // This line sets the RTC with an explicit date & time, for example to set
+  //   // January 21, 2014 at 3am you would call:
+  //   // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  // }
 
   // Serial1.println("Hello, Raspberry Pi Pico W!");
 }
 
-unsigned int counter = 0;
+unsigned int counter = 0;  
+DateTime now = rtc.now();
 
 void loop() {
-  DateTime now = rtc.now();
+  delay(1); // this speeds up the simulation
 
   tm.display(0, ((now.hour(), DEC) / 10) % 10);
   tm.display(1, (now.hour(), DEC) % 10);
   tm.display(2, ((now.minute(), DEC) / 10) % 10);
   tm.display(3, (now.minute(), DEC) % 10);
 
-  // print time, for debugging 
-  Serial.print("Current time: ");
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
+  // // print time, for debugging 
+  // Serial.print("Current time: ");
+  // Serial.print(now.year(), DEC);
+  // Serial.print('/');
+  // Serial.print(now.month(), DEC);
+  // Serial.print('/');
+  // Serial.print(now.day(), DEC);
+  // Serial.print(now.hour(), DEC);
+  // Serial.print(':');
+  // Serial.print(now.minute(), DEC);
+  // Serial.print(':');
+  // Serial.print(now.second(), DEC);
+  // Serial.println();
+
+
 
   // put your main code here, to run repeatedly:
-  delay(1); // this speeds up the simulation
 
-  tm.display(0, (counter / 1000) % 10);
-  tm.display(1, (counter / 100) % 10);
-  tm.display(2, (counter / 10) % 10);
-  tm.display(3, counter % 10);
+  // tm.display(0, (counter / 1000) % 10);
+  // tm.display(1, (counter / 100) % 10);
+  // tm.display(2, (counter / 10) % 10);
+  // tm.display(3, counter % 10);
 
-  counter++;
-  if (counter == 10000) {
-    counter = 0;
-  }
+  // counter++;
+  // if (counter == 10000) {
+  //   counter = 0;
+  // }
 
-  delay(1000);
+  // delay(1000);
 
 }
