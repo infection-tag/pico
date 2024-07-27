75294b7fdac6067b3a8eaef187969f08eae0f044
NiKo Kompella <63625756+infection-tag@users.noreply.github.com>
2024-07-26 18:12:03 -0700

    Update sketch.ino

--- the-bin-2-2/sketch.ino
+++ the-bin-2-2/sketch.ino
@@ -3,12 +3,17 @@
 #include <TM1637.h>
 #include <Wire.h> // Include for I2C communication
 #include <RTClib.h> // Include for RTC library
+#include <U8g2lib.h>
+#include <Bounce2.h>
+#include <Tiny4kOLED.h>
+#include "ssd1306.h"
 
 #define SCL 2  // Define clock pin for display
 #define SDA 4  // Define data pin for display
 #define LED 13 // Define LED pin
 
 RTC_DS1307 rtc; // Create an RTC object
+U8GLIB_SSD1306_128X64 display(U8G_I2C_OPT_DEV_0 | U8G_I2C_OPT_NO_ACK | U8G_I2C_OPT_FAST); // Fast I2C / TWI
 
 void setup() {
   Serial.begin(57600);
@@ -31,23 +36,20 @@ int lastHour = -1; // Variable to store previous hour
 void loop() {
   DateTime now = rtc.now();
 
-  // // Display time on the 4-digit display
-  // display.display(0, (hour / 10) % 10); // Tens digit of hour
-  // display.display(1, hour % 10);       // Ones digit of hour
-  // display.display(2, (minute / 10) % 10); // Tens digit of minute
-  // display.display(3, minute % 10);       // Ones digit of minute
-  // // TODO: Show colon between hours and minutes
+  int hour = now.hour();
 
-  // // Hourly chime with LED
-  // if (hour != lastHour) {
-  //   for (int i = 0; i < hour; i++) { // Blink LED for each hour
-  //     digitalWrite(LED, HIGH);
-  //     delay(250);
-  //     digitalWrite(LED, LOW);
-  //     delay(250);
-  //   }
-  //   lastHour = hour; // Update last hour for comparison
-  // }
+  
+
+  // Hourly chime with LED
+  if (hour != lastHour) {
+    for (int i = 0; i < hour; i++) { // Blink LED for each hour
+      digitalWrite(LED, HIGH);
+      delay(250);
+      digitalWrite(LED, LOW);
+      delay(250);
+    }
+    lastHour = hour; // Update last hour for comparison
+  }
 
   Serial.print(now.hour());
 
